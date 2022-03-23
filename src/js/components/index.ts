304c6c1081d4d68aa01293a3dc03ee3cef39e703
NaotoshiFujita <fujita.naotoshi@gmail.com>
2022-03-23 09:25:31 +0900

    Fix failure of rebase.

--- src/js/components/index.ts
+++ src/js/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Splide } from './Splide/Splide.vue';
 export { default as SplideTrack } from './SplideTrack/SplideTrack.vue';
 export { default as SplideSlide } from './SplideSlide/SplideSlide.vue';
-export { default as SplideTrack } from './SplideTrack/SplideTrack.vue';
